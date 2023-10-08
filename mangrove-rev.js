c8c2d4380bb03e389a9fee2edd2dc73545f238e5
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-10-08 22:56:29 +0200

    Test mangrove typescript

--- mangrove-rev.js
+++ mangrove-rev.js
@@ -1,6 +1,13 @@
-import { getReviews } from "mangrove-reviews";
+// Run this: node mangrove-rev.js
 
-// Of a particular subject.
-const subReviews = await getReviews({ sub: "geo:51.4954589,11.9659188", q: "verwöner", u: 30, issuers: true, maresi_subjects: true });
+// "mangrove-reviews"
+// import { getReviews } from "mangrove-reviews";
+//
+// const subReviews = await getReviews({ sub: "geo:51.4954589,11.9659188", q: "verwöner", u: 30, issuers: true, maresi_subjects: true });
+
+// "mangrove-reviews-typescript"
+import { MangroveReviews } from "mangrove-reviews-typescript";
+
+const subReviews = await MangroveReviews.getReviews({ sub: "geo:51.4954589,11.9659188", q: "verwöner", u: 30, issuers: true, maresi_subjects: true });
 
 console.log(subReviews.reviews);
