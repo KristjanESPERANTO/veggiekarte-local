5fa4e7c1ec51fe942d5d0a6e044de6ae1da0215c
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-11-11 08:12:56 +0100

    Use rollup to bundle

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import banner2 from "rollup-plugin-banner2";
+import terser from "@rollup/plugin-terser";
+import pkg from "./package.json" assert { type: "json" };
+
+const bannerText = `/*! *****************************************************************************
+  ${pkg.name}
+  Version ${pkg.version}
+
+  ${pkg.description}
+  Please submit bugs at ${pkg.bugs.url}
+
+  (c) ${pkg.author.name ? pkg.author.name : pkg.contributors}
+  Licence: ${pkg.license}
+
+  This file is auto-generated. Do not edit.
+***************************************************************************** */
+`;
+
+export default {
+  input: "js/veggiemap.js",
+  output: {
+    file: "js/bundle.js",
+    format: "iife"
+  },
+  plugins: [terser(), banner2(() => bannerText)]
+};
