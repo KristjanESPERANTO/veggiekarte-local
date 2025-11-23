e6de7d71b16134cf0ce5dd33f7ae45267ee4f3fa
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-11-23 15:31:27 +0100

    refactor: rework icon system
    - Replace OpenStreetMap Carto icons with Maki and Temaki
    - Generate CSS with embedded SVG data URIs instead of external files
    - Add new icon mappings (cosmetics, deli, farm, florist, grocery, herbalist, tea, nutrition_supplements)

--- js/veggiemap-icons.js
+++ js/veggiemap-icons.js
@@ -1,43 +1,11 @@
-import { Icon } from "leaflet";
-
-/* Variables for the icon color */
-const whiteIconColor = "-white.svg";
-const blackIconColor = "-black.svg";
+import { DivIcon } from "leaflet";
 
 /* Function to get the icon depending from the symbol and the category */
 export default function getIcon(symbol, category) {
-  let iconColor;
-  if (category === "vegan_only") {
-    iconColor = whiteIconColor;
-  }
-  else if (category === "vegetarian_only") {
-    iconColor = whiteIconColor;
-  }
-  else if (category === "vegan_friendly") {
-    iconColor = blackIconColor;
-  }
-  else if (category === "vegan_limited") {
-    iconColor = blackIconColor;
-  }
-  else if (category === "vegetarian_friendly") {
-    iconColor = blackIconColor;
-  }
-
-  /* Check if it's a maki or osm-carto icon */
-  let iconPath;
-  if (symbol.startsWith("maki_")) {
-    iconPath = "third-party/icons/maki/";
-    symbol = symbol.replace("maki_", "");
-  }
-  else {
-    iconPath = "third-party/icons/openstreetmap-carto/";
-  }
-
-  return new Icon({
-    iconUrl: iconPath + symbol + iconColor,
+  return new DivIcon({
+    className: `marker ${category} icon-${symbol}`,
     iconSize: [18, 18],
     iconAnchor: [11, 18],
-    popupAnchor: [0, -18],
-    className: `marker ${category}`
+    popupAnchor: [0, -18]
   });
 }
